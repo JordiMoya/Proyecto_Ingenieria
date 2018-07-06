de967ecc0cc7aba4eb6d71e5bdd132fa0529aa43
asfedez <solanofedezadrian@gmail.com>
2018-07-06 01:06:24 -0600

    Factura Ingreso

--- Capa_Presentacion/frmFacturaIngreso.cs
+++ Capa_Presentacion/frmFacturaIngreso.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Capa_Negocio;
+using Entidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,7 +21,70 @@ namespace Capa_Presentacion
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //esto es una prueba
+            try
+            {
+                FacturaIngreso facturaIngreso = new FacturaIngreso
+                {
+                    NumeroFacturaIngreso = Convert.ToInt32(txtFacturaIngreso.Text),
+                    NumeroFactura = Convert.ToInt32(txtFactura.Text),
+                    Fecha = Convert.ToDateTime(dtpFecha.Text),
+                    Semana = Convert.ToInt32(txtSemana.Text),
+                    Descripcion = rtxtDescripcion.Text,
+                    Total = Convert.ToInt32(txtTotal.Text)
+                };
+
+                new Cls_FacturaIngreso().Agregar(facturaIngreso);
+                MessageBox.Show("Factura guardada exitosamente", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FacturaIngreso facturaIngreso = new Cls_FacturaIngreso().Consultar(Convert.ToInt32(txtFacturaIngreso.Text));
+                txtFactura.Text = "" + facturaIngreso.NumeroFactura;
+                dtpFecha.Text = "" + facturaIngreso.Fecha;
+                txtSemana.Text = "" + facturaIngreso.Semana;
+                rtxtDescripcion.Text = "" + facturaIngreso.Descripcion;
+                txtTotal.Text = "" + facturaIngreso.Total;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                FacturaIngreso facturaIngreso = new FacturaIngreso
+                {
+                    NumeroFacturaIngreso = Convert.ToInt32(txtFacturaIngreso.Text),
+                    NumeroFactura = Convert.ToInt32(txtFactura.Text),
+                    Fecha = Convert.ToDateTime(dtpFecha.Text),
+                    Semana = Convert.ToInt32(txtSemana.Text),
+                    Descripcion = rtxtDescripcion.Text,
+                    Total = Convert.ToInt32(txtTotal.Text)
+                };
+
+                new Cls_FacturaIngreso().Modificar(facturaIngreso);
+                MessageBox.Show("Factura Modificada correctamente", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
