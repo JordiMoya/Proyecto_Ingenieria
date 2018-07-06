4ad6b0b6f7e5068ea58a0ac6b3eb7d37c5286fff
María Guadalupe Salazar Alaniz <luperuz17@gmail.com>
2018-07-06 00:29:47 -0600

    frmControlHoras

--- Capa_Presentacion/frmUsuarios.Designer.cs
+++ Capa_Presentacion/frmUsuarios.Designer.cs
@@ -30,10 +30,6 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmUsuarios));
             this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.btnConsultar = new System.Windows.Forms.Button();
-            this.btnEliminar = new System.Windows.Forms.Button();
-            this.btnModificar = new System.Windows.Forms.Button();
-            this.btnAgregar = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.txtConfirContra = new System.Windows.Forms.TextBox();
             this.txtContra = new System.Windows.Forms.TextBox();
@@ -41,6 +37,10 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
             this.groupBox4.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -57,6 +57,72 @@
             this.groupBox4.TabIndex = 8;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Acciones";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.txtConfirContra);
+            this.groupBox1.Controls.Add(this.txtContra);
+            this.groupBox1.Controls.Add(this.txtNomUsuario);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(26, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(372, 144);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Información Usuario";
+            // 
+            // txtConfirContra
+            // 
+            this.txtConfirContra.Location = new System.Drawing.Point(135, 98);
+            this.txtConfirContra.Name = "txtConfirContra";
+            this.txtConfirContra.PasswordChar = '*';
+            this.txtConfirContra.Size = new System.Drawing.Size(143, 20);
+            this.txtConfirContra.TabIndex = 5;
+            // 
+            // txtContra
+            // 
+            this.txtContra.Location = new System.Drawing.Point(135, 62);
+            this.txtContra.Name = "txtContra";
+            this.txtContra.PasswordChar = '*';
+            this.txtContra.Size = new System.Drawing.Size(143, 20);
+            this.txtContra.TabIndex = 4;
+            // 
+            // txtNomUsuario
+            // 
+            this.txtNomUsuario.Location = new System.Drawing.Point(135, 29);
+            this.txtNomUsuario.Name = "txtNomUsuario";
+            this.txtNomUsuario.Size = new System.Drawing.Size(143, 20);
+            this.txtNomUsuario.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Confirmar Contraseña:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contraseña:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre Usuario:";
             // 
             // btnConsultar
             // 
@@ -117,72 +183,6 @@
             this.btnAgregar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnAgregar.UseVisualStyleBackColor = false;
             this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.BackColor = System.Drawing.Color.White;
-            this.groupBox1.Controls.Add(this.txtConfirContra);
-            this.groupBox1.Controls.Add(this.txtContra);
-            this.groupBox1.Controls.Add(this.txtNomUsuario);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(26, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(372, 144);
-            this.groupBox1.TabIndex = 7;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Información Usuario";
-            // 
-            // txtConfirContra
-            // 
-            this.txtConfirContra.Location = new System.Drawing.Point(135, 98);
-            this.txtConfirContra.Name = "txtConfirContra";
-            this.txtConfirContra.PasswordChar = '*';
-            this.txtConfirContra.Size = new System.Drawing.Size(143, 20);
-            this.txtConfirContra.TabIndex = 5;
-            // 
-            // txtContra
-            // 
-            this.txtContra.Location = new System.Drawing.Point(135, 62);
-            this.txtContra.Name = "txtContra";
-            this.txtContra.PasswordChar = '*';
-            this.txtContra.Size = new System.Drawing.Size(143, 20);
-            this.txtContra.TabIndex = 4;
-            // 
-            // txtNomUsuario
-            // 
-            this.txtNomUsuario.Location = new System.Drawing.Point(135, 29);
-            this.txtNomUsuario.Name = "txtNomUsuario";
-            this.txtNomUsuario.Size = new System.Drawing.Size(143, 20);
-            this.txtNomUsuario.TabIndex = 3;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(28, 101);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(108, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Confirmar Contraseña";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 65);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Contraseña";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nombre Usuario";
             // 
             // frmUsuarios
             // 
